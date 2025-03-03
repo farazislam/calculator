13899712236ba26a121bcf594919c535b47240d2
Faraz <farazul.islam@gmail.com>
2025-03-03 01:00:53 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let leftValue;
+let rightValue;
+let operator;
+
+function add(a, b) {
+    return (a+b)
+}
+
+function subtract(a, b) {
+    return (a-b)
+}
+
+function multiply(a, b) {
+    return (a*b)
+}
+
+function divide (a, b) {
+    return (a/b)
+}
+
+function operate(func, a, b) {
+    return func(a, b)
+}
