60390477c9ffc99f7852934d626cc510b04f8275
Faraz <farazul.islam@gmail.com>
2025-03-04 22:23:48 +0000

    Add event listeners to numbers

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+let display  = document.querySelector('.display')
+let numbersNodeList = document.querySelectorAll('.number')
+let numbers = Array.from(numbersNodeList)
 let leftValue;
 let rightValue;
-let operator;
+let operator = '+';
+
+// Add event listeners to numbers
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if(display.innerText==='0') {
+            display.innerText = e.srcElement.attributes.value.value
+        } else {
+            display.innerText += e.srcElement.attributes.value.value
+        }
+
+        if(operator===null) {
+            leftValue = Number(display.innerText)
+        } else {
+            rightValue = Number(display.innerText)
+        }
+    })
+})
+
 
 function add(a, b) {
     return (a+b)
